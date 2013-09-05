8d9156108670c8f573625ea7a28dce746e553629
cormacpayne <cormacpayne@ufl.edu>
2013-09-05 13:15:28 -0400

    Create HANGOVER.java

---
+++ HANGOVER/HANGOVER.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+class HACKRNDM{
+	public static void main(String[] args) throws Exception{
+		Parser in = new Parser(System.in);
+		int n = in.nextInt();
+		int k = in.nextInt();
+		int count = 0;
+		int[] array = new int[n];
+		for(int x = 0; x < n; x++){
+			array[x] = in.nextInt();
+		}
+		Arrays.sort(array);
+		for(int y = 0; y < n; y++){
+			if(Arrays.binarySearch(array, array[y]+k) >= 0){
+				count++;
+			}
+		}
+		System.out.print(count);
+	}
+
+
+}
+
+class Parser
+{
+   final private int BUFFER_SIZE = 1 << 16;
+
+   private DataInputStream din;
+   private byte[] buffer;
+   private int bufferPointer, bytesRead;
+
+   public Parser(InputStream in)
+   {
+      din = new DataInputStream(in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+   }
+
+   public long nextLong() throws Exception
+   {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   //reads in the next string
+   public String next() throws Exception
+   {
+      StringBuilder ret =  new StringBuilder();
+      byte c = read();
+      while (c <= ' ') c = read();
+      do
+      {
+         ret = ret.append((char)c);
+         c = read();
+      } while (c > ' ');
+      return ret.toString();
+   }
+
+   public int nextInt() throws Exception
+   {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   private void fillBuffer() throws Exception
+   {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+   }
+
+   private byte read() throws Exception
+   {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+   }
+}
