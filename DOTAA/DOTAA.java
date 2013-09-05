bcd18b18f912b62e29075af7581664e9e257a64e
cormacpayne <cormacpayne@ufl.edu>
2013-09-04 22:27:55 -0400

    Create DOTAA.java

---
+++ DOTAA/DOTAA.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.*;
+class DOTAA {
+	public static void main(String[] args)throws Exception{
+		Parser in = new Parser(System.in);
+		StringBuilder string = new StringBuilder();
+		int t = in.nextInt();
+		for(int i = 0; i < t; i++){
+			int n = in.nextInt();
+			int towers = in.nextInt();
+			int D = in.nextInt();
+			for(int j = 0; j < n; j++){
+				int s = in.nextInt();
+				for(;;){
+					s-=D;
+					if(s > 0){
+						towers--;
+					}else{
+						break;
+					}
+				}
+			}
+			if(towers < 1){
+				string.append("YES\n");
+			}else{
+				string.append("NO\n");
+			}
+		}
+		System.out.print(string);
+	}
+}
+
+class Parser
+{
+   final private int BUFFER_SIZE = 1 << 16;
+
+   private DataInputStream din;
+   private byte[] buffer;
+   private int bufferPointer, bytesRead;
+
+   public Parser(InputStream in)
+   {
+      din = new DataInputStream(in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+   }
+
+   public long nextLong() throws Exception
+   {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   //reads in the next string
+   public String next() throws Exception
+   {
+      StringBuilder ret =  new StringBuilder();
+      byte c = read();
+      while (c <= ' ') c = read();
+      do
+      {
+         ret = ret.append((char)c);
+         c = read();
+      } while (c > ' ');
+      return ret.toString();
+   }
+
+   public int nextInt() throws Exception
+   {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   private void fillBuffer() throws Exception
+   {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+   }
+
+   private byte read() throws Exception
+   {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+   }
+}
