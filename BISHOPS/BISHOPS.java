55b6fc35b4feddaf28380ff272e58f7b56fb6111
cormacpayne <cormacpayne@ufl.edu>
2013-09-04 22:03:07 -0400

    Create BISHOPS.java

---
+++ BISHOPS/BISHOPS.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.math.*;
+class BISHOPS {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		StringBuilder string = new StringBuilder();
+		while(in.hasNext()){
+			BigInteger num = in.nextBigInteger();
+			if(num.intValue() == 1){
+				string.append("1\n");
+			}else{
+				num = num.subtract(BigInteger.ONE);
+				BigInteger result = num.multiply(BigInteger.valueOf(2));
+				string.append(result + "\n");
+			}
+		}
+		System.out.println(string);
+	}
+}
