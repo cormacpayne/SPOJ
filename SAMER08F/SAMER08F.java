fdb61bd2d1b07c8245e0ad3337e991d9a9c1e725
cormacpayne <cormacpayne@ufl.edu>
2013-09-05 14:27:18 -0400

    Create SAMER08F.java

---
+++ SAMER08F/SAMER08F.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+class SAMER08F {
+	public static void main(String[] args) throws Exception{
+		Parser in = new Parser(System.in);
+		StringBuilder string = new StringBuilder();
+		int num = in.nextInt();
+		while(num != 0){
+			int total = 0;
+			for(int x = num; x >= 1; x--){
+				total += x * x;
+			}
+			string.append(total + "\n");
+			num = in.nextInt();
+		}
+		System.out.println(string);
+	}
+}
+	
+	//you need to import java.io and throw exceptions in the methods that call nextInt()
+
+	class Parser{
+	   final private int BUFFER_SIZE = 1 << 16;
+	 
+	   private DataInputStream din;
+	   private byte[] buffer;
+	   private int bufferPointer, bytesRead;
+	 
+	   public Parser(InputStream in){
+	      din = new DataInputStream(in);
+	      buffer = new byte[BUFFER_SIZE];
+	      bufferPointer = bytesRead = 0;
+	   }
+	 
+	   public int nextInt() throws Exception{
+	      int ret = 0;
+	      byte c = read();
+	      while (c <= ' ') c = read();
+	      boolean neg = c == '-';
+	      if (neg) c = read();
+	      do
+	      {
+	         ret = ret * 10 + c - '0';
+	         c = read();
+	      } while (c > ' ');
+	      if (neg) return -ret;
+	      return ret;
+	   }
+	 
+	   private void fillBuffer() throws Exception{
+	      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+	      if (bytesRead == -1) buffer[0] = -1;
+	   }
+	 
+	   private byte read() throws Exception{
+	      if (bufferPointer == bytesRead) fillBuffer();
+	      return buffer[bufferPointer++];
+	   }
+	}
