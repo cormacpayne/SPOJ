91122c5a28e11d78cfb68667c6ced2abd5df1d8d
cormacpayne <cormacpayne@ufl.edu>
2013-09-05 13:19:26 -0400

    Create HPYNOS.java

---
+++ HPYNOS/HPYNOS.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.util.*;
+class HPYNOS{
+	public static void main(String[] args) throws Exception{
+		Parser in = new Parser(System.in);
+		List<Integer> nums = new ArrayList<Integer>();
+		String n = in.next();
+		int count = 0;
+		while(true){
+			int sum = 0;
+			for(int x = 0; x < n.length(); x++){
+				sum += Integer.parseInt(n.substring(x, x+1))*Integer.parseInt(n.substring(x, x+1));
+			}
+			if(nums.contains(sum)){
+				count = -1;
+				break;
+			}else if(sum == 1){
+				count++;
+				break;
+			}else{
+				nums.add(sum);
+				n = Integer.toString(sum);
+				count++;
+			}
+		}
+		System.out.print(count);
+	}
+}
+
+class Parser
+{
+   final private int BUFFER_SIZE = 1 << 16;
+
+   private DataInputStream din;
+   private byte[] buffer;
+   private int bufferPointer, bytesRead;
+
+   public Parser(InputStream in)
+   {
+      din = new DataInputStream(in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+   }
+
+   public long nextLong() throws Exception
+   {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   //reads in the next string
+   public String next() throws Exception
+   {
+      StringBuilder ret =  new StringBuilder();
+      byte c = read();
+      while (c <= ' ') c = read();
+      do
+      {
+         ret = ret.append((char)c);
+         c = read();
+      } while (c > ' ');
+      return ret.toString();
+   }
+
+   public int nextInt() throws Exception
+   {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   private void fillBuffer() throws Exception
+   {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+   }
+
+   private byte read() throws Exception
+   {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+   }
+}
