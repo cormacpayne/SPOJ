cb8ffd219eafeda7f2b3fd6c552a56bbb3a70aff
cormacpayne <cormacpayne@ufl.edu>
2013-09-04 22:45:56 -0400

    Create FENCE1.java

---
+++ FENCE1/FENCE1.java
@@ -0,0 +1,53 @@
+import java.io.*;
+class FENCE1 {
+	public static void main(String[] args) throws Exception{
+		Parser in = new Parser(System.in);
+		int num = in.nextInt();
+		while(num != 0){			
+			double area = (int)(100 *((Math.pow((((num * 2)/Math.PI)/2), 2) * Math.PI)/2) + 0.5)/(double)100;
+			System.out.printf("%.2f\n", area);
+			num = in.nextInt();
+		}
+	}
+}
+	
+	//you need to import java.io and throw exceptions in the methods that call nextInt()
+
+	class Parser{
+	   final private int BUFFER_SIZE = 1 << 16;
+	 
+	   private DataInputStream din;
+	   private byte[] buffer;
+	   private int bufferPointer, bytesRead;
+	 
+	   public Parser(InputStream in){
+	      din = new DataInputStream(in);
+	      buffer = new byte[BUFFER_SIZE];
+	      bufferPointer = bytesRead = 0;
+	   }
+	 
+	   public int nextInt() throws Exception{
+	      int ret = 0;
+	      byte c = read();
+	      while (c <= ' ') c = read();
+	      boolean neg = c == '-';
+	      if (neg) c = read();
+	      do
+	      {
+	         ret = ret * 10 + c - '0';
+	         c = read();
+	      } while (c > ' ');
+	      if (neg) return -ret;
+	      return ret;
+	   }
+	 
+	   private void fillBuffer() throws Exception{
+	      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+	      if (bytesRead == -1) buffer[0] = -1;
+	   }
+	 
+	   private byte read() throws Exception{
+	      if (bufferPointer == bytesRead) fillBuffer();
+	      return buffer[bufferPointer++];
+	   }
+	}
