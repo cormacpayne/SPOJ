cbf80c43e87da891b70cf40dece811d984fd5725
cormacpayne <cormacpayne@ufl.edu>
2013-09-05 14:47:46 -0400

    Create TRGRID.java

---
+++ TRGRID/TRGRID.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+class TRGRID {
+
+	public static void main(String[] args) throws Exception{
+
+		Parser in = new Parser(System.in);
+		StringBuilder string = new StringBuilder();
+		int num = in.nextInt();
+		for(int x = 0; x < num; x++){
+			int n = in.nextInt();
+			int m = in.nextInt();
+			
+			if(m == n){
+				if(m%2 == 0){
+					string.append("L\n");
+				}else{
+					string.append("R\n");
+				}
+			}else if(m < n){
+				if(m%2 == 0){
+					string.append("U\n");
+				}else{
+					string.append("D\n");
+				}
+			}else{
+				if(n%2 == 0){
+					string.append("L\n");
+				}else{
+					string.append("R\n");
+				}
+			}
+		}
+		System.out.println(string);
+	}
+}
+
+
+
+	
+	//you need to import java.io and throw exceptions in the methods that call nextInt()
+
+	class Parser{
+	   final private int BUFFER_SIZE = 1 << 16;
+	 
+	   private DataInputStream din;
+	   private byte[] buffer;
+	   private int bufferPointer, bytesRead;
+	 
+	   public Parser(InputStream in){
+	      din = new DataInputStream(in);
+	      buffer = new byte[BUFFER_SIZE];
+	      bufferPointer = bytesRead = 0;
+	   }
+	 
+	   public int nextInt() throws Exception{
+	      int ret = 0;
+	      byte c = read();
+	      while (c <= ' ') c = read();
+	      boolean neg = c == '-';
+	      if (neg) c = read();
+	      do
+	      {
+	         ret = ret * 10 + c - '0';
+	         c = read();
+	      } while (c > ' ');
+	      if (neg) return -ret;
+	      return ret;
+	   }
+	 
+	   private void fillBuffer() throws Exception{
+	      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+	      if (bytesRead == -1) buffer[0] = -1;
+	   }
+	 
+	   private byte read() throws Exception{
+	      if (bufferPointer == bytesRead) fillBuffer();
+	      return buffer[bufferPointer++];
+	   }
+	}
