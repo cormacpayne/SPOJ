ef05e5d617cc2fbfe226f4610ed6894c7b33bd76
cormacpayne <cormacpayne@ufl.edu>
2013-09-04 22:09:17 -0400

    Create BYTESM2.java

---
+++ BYTESM2/BYTESM2.java
@@ -0,0 +1,113 @@
+import java.io.*;
+class BYTESM2 {
+	public static void main(String[] args) throws Exception{
+		Parser in = new Parser(System.in);
+		StringBuilder string = new StringBuilder();
+		int[][] grid = new int[102][102];
+		int t = in.nextInt();
+		for(int x = 0; x < t; x++){
+			int rows = in.nextInt();
+			int cols = in.nextInt();
+			int i, j;
+			for(i = 1; i <= rows; i++){
+				for(j = 1; j <= cols; j++){
+					grid[i][j] = in.nextInt();
+				}
+				grid[i][j] = 0;
+				for(j = 1; j <= cols; j++){
+					grid[i][j] += max(grid[i-1][j-1], grid[i-1][j], grid[i-1][j+1]);
+				}				
+			}
+			int r = grid[rows][1];
+			for(i = 2; i <= cols; i++){
+				r = max(r, grid[rows][i], 0);
+			}
+			string.append(r + "\n");
+		}
+		System.out.print(string);
+	}
+	
+	public static int max(int a, int b, int c){
+		if(a >= b && a >= c){
+			return a;
+		}else if(b >= c){
+			return b;
+		}else{
+			return c;
+		}
+	}
+}
+
+class Parser
+{
+   final private int BUFFER_SIZE = 1 << 16;
+
+   private DataInputStream din;
+   private byte[] buffer;
+   private int bufferPointer, bytesRead;
+
+   public Parser(InputStream in)
+   {
+      din = new DataInputStream(in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+   }
+
+   public long nextLong() throws Exception
+   {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   //reads in the next string
+   public String next() throws Exception
+   {
+      StringBuilder ret =  new StringBuilder();
+      byte c = read();
+      while (c <= ' ') c = read();
+      do
+      {
+         ret = ret.append((char)c);
+         c = read();
+      } while (c > ' ');
+      return ret.toString();
+   }
+
+   public int nextInt() throws Exception
+   {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   private void fillBuffer() throws Exception
+   {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+   }
+
+   private byte read() throws Exception
+   {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+   }
+}
