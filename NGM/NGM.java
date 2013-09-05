ee06982e4bf7bc651176122e9f89484423312ee4
cormacpayne <cormacpayne@ufl.edu>
2013-09-05 13:39:04 -0400

    Create NGM.java

---
+++ NGM/NGM.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+class NGM{
+	public static void main(String[] args) throws Exception{
+		Parser in = new Parser(System.in);
+		int n = in.nextInt();
+		if(n%10 == 0){
+			System.out.print("2");
+		}else{
+			System.out.print("1\n" + n%10);
+		}
+	}
+}
+
+class Parser
+{
+   final private int BUFFER_SIZE = 1 << 16;
+
+   private DataInputStream din;
+   private byte[] buffer;
+   private int bufferPointer, bytesRead;
+
+   public Parser(InputStream in)
+   {
+      din = new DataInputStream(in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+   } 
+   
+   public int nextInt() throws Exception
+   {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   private void fillBuffer() throws Exception
+   {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+   }
+
+   private byte read() throws Exception
+   {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+   }
+}
