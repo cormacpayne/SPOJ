bc12d5d4baa4ac7ea5e5b933d9914c65fb35c11f
cormacpayne <cormacpayne@ufl.edu>
2013-09-04 22:02:04 -0400

    Create BEENUMS.java

---
+++ BEENUMS/BEENUMS.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+class BEENUMS{
+    public static void main(String[] args) throws Exception{
+        Parser in = new Parser(System.in);
+        StringBuilder string = new StringBuilder();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int count = 0;
+        int num = 1;
+        while(num < 1000000000){
+            num += count;
+            count += 6;
+            list.add(num);
+        }   
+        int n = in.nextInt();
+        while(n != -1){
+            if(list.contains(n)){
+                string.append("Y\n");
+            }else{
+                string.append("N\n");
+            }
+            n = in.nextInt();
+        }
+        System.out.print(string);
+    }   
+}
+
+class Parser
+{
+   final private int BUFFER_SIZE = 1 << 16;
+
+   private DataInputStream din;
+   private byte[] buffer;
+   private int bufferPointer, bytesRead;
+
+   public Parser(InputStream in)
+   {
+      din = new DataInputStream(in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+   }
+
+   public long nextLong() throws Exception
+   {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   //reads in the next string
+   public String next() throws Exception
+   {
+      StringBuilder ret =  new StringBuilder();
+      byte c = read();
+      while (c <= ' ') c = read();
+      do
+      {
+         ret = ret.append((char)c);
+         c = read();
+      } while (c > ' ');
+      return ret.toString();
+   }
+
+   public int nextInt() throws Exception
+   {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   private void fillBuffer() throws Exception
+   {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+   }
+
+   private byte read() throws Exception
+   {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+   }
+}
