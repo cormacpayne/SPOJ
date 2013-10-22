6b8f18d8b58636b05edc78be0a5c2ad4b6140a2c
cormacpayne <cormacpayne@ufl.edu>
2013-10-22 16:02:13 -0400

    Create UCV2013J.java

---
+++ UCV2013J/UCV2013J.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+class UCV2013J {
+	public static void main(String[] args)throws Exception{
+		Parser in = new Parser(System.in);
+		StringBuilder string = new StringBuilder();
+		int vertices = in.nextInt();
+		while(vertices != 0){
+			int num = (vertices+1)/2;
+			int wall = vertices-num;
+			long total = 0;
+			for(int i = 0; i < vertices; i++){
+				int j = in.nextInt();
+				if(i >= wall){
+					total += j;
+				}
+			}
+			string.append(total + "\n");
+			vertices = in.nextInt();
+		}
+		System.out.print(string);
+	}
+}
+
+class Parser
+{
+   final private int BUFFER_SIZE = 1 << 16;
+
+   private DataInputStream din;
+   private byte[] buffer;
+   private int bufferPointer, bytesRead;
+
+   public Parser(InputStream in)
+   {
+      din = new DataInputStream(in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+   }
+
+   public long nextLong() throws Exception
+   {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   //reads in the next string
+   public String next() throws Exception
+   {
+      StringBuilder ret = new StringBuilder();
+      byte c = read();
+      while (c <= ' ') c = read();
+      do
+      {
+         ret = ret.append((char)c);
+         c = read();
+      } while (c > ' ');
+      return ret.toString();
+   }
+
+   public int nextInt() throws Exception
+   {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = c == '-';
+      if (neg) c = read();
+      do
+      {
+         ret = ret * 10 + c - '0';
+         c = read();
+      } while (c > ' ');
+      if (neg) return -ret;
+      return ret;
+   }
+   
+   private void fillBuffer() throws Exception
+   {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+   }
+
+   private byte read() throws Exception
+   {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+   }
+}
