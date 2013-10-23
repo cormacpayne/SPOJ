d74062e8e9a9a24f8b5d6f0417e90d8ce0830333
cormacpayne <cormacpayne@ufl.edu>
2013-10-22 23:10:55 -0400

    Update NAKANJ.java

--- NAKANJ/NAKANJ.java
+++ NAKANJ/NAKANJ.java
@@ -1,3 +1,60 @@
+import java.io.*;
+import java.util.*;
+class NAKANJ {
+	public static void main(String[] args)throws Exception{
+		Parser in = new Parser(System.in);
+		StringBuilder string = new StringBuilder();
+		int t = in.nextInt();
+		for(int i = 0; i < t; i++){
+			boolean[][] visited = new boolean[8][8];
+			String start = in.next();
+			String end = in.next();
+			
+			int startX = start.charAt(0) - 'a';
+			int startY = Integer.parseInt(start.substring(1))-1;
+			
+			int endX = end.charAt(0) - 'a';
+			int endY = Integer.parseInt(end.substring(1))-1;
+			
+			Queue<Point> q = new LinkedList<Point>();
+			Point current = null;
+			int[] xValues = {2, 2, -2, -2, 1, 1, -1, -1};
+			int[] yValues = {1, -1, 1, -1, 2, -2, 2, -2};
+			q.add(new Point(startX, startY, 0));
+			
+			while(!q.isEmpty()){
+				current = q.remove();
+				if(current.x == endX && current.y == endY){
+					break;
+				}
+				
+				for(int j = 0; j < 8; j++){
+					int x = current.x + xValues[j];
+					int y = current.y + yValues[j];
+					
+					if(x >= 0 && x < 8 && y >= 0 && y < 8 && !visited[x][y]){
+						visited[x][y] = true;
+						q.add(new Point(x, y, current.dist+1));
+					}
+				}
+			}
+			
+			string.append(current.dist + "\n");
+		}
+		System.out.print(string);
+	}
+	
+	static class Point{
+		int x,y,dist;
+		
+		public Point(int x, int y, int dist){
+			this.x = x;
+			this.y = y;
+			this.dist = dist;
+		}
+	}
+}
+
 class Parser
 {
    final private int BUFFER_SIZE = 1 << 16;
